3ef78b0384c17a5e6c2a2e5498dc9daa4d795570
Kenneth Chuck <krchuck@outlook.com>
2019-12-12 15:44:15 -0700

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { NavLink, } from "react-router-dom"
+import { Menu, } from "semantic-ui-react"
+
+const Navbar = () => (
+  <Menu>
+    <NavLink to="/">
+      <Menu.Item>Home</Menu.Item>
+    </NavLink>
+    <NavLink to="/user/profile">
+      <Menu.Item>
+        {/* Will be replaced with the actual username */}
+        Profile
+      </Menu.Item>
+    </NavLink>
+  </Menu>
+);
+
+export default Navbar
